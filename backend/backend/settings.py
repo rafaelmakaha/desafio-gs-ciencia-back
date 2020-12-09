25732399d8fec7c8544ee853b67fa87f31816d21
Rafael Makaha <rafael.makaha@gmail.com>
2020-12-09 01:43:23 -0300

    Remove unused imports.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -135,3 +135,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3001",
+    "http://localhost:3000"
+]
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
--- backend/product/views.py
+++ backend/product/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
